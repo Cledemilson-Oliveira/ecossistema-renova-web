3d46e9c9c623a8dfa0d606fc08155dbe9369b5d4
Cledemilson Oliveira de Assis <consultorcledemilsonoliveira@gmail.com>
2026-09-14 00:46:21 -0300

    chore: define dominio oficial renova.servicosgold.com.br

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const RENOVA_CONFIG = Object.freeze({
   supabaseUrl: 'https://nnjvxomaermffqnmwtzr.supabase.co',
   supabasePublishableKey: 'sb_publishable_N4BznMYx8cvNxffvm-BWZg__MW7xV8g',
-  siteUrl: 'https://ecossistemarenova.servicosgold.com.br',
+  siteUrl: 'https://renova.servicosgold.com.br',
   githubPagesUrl: 'https://cledemilson-oliveira.github.io/ecossistema-renova-web/',
   brand: {
     name: 'Ecossistema RENOVA',
